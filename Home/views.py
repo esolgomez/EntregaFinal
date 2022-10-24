7dba8e3f336c6553bcf5ef7937c348b33ef8f79d
Soledad Gomez <esolgomez@gmail.com>
2022-10-24 08:54:24 -0300

    Cuarto commit del proyecto

--- Home/views.py
+++ Home/views.py
@@ -3,24 +3,45 @@ from datetime import datetime
 # from django.template import Context, Template, loader
 from django.shortcuts import render, redirect
 import random
+from Home.forms import BusquedaHumanoFormulario, HumanoFormulario
 
 from Home.models import Humano
 
 def crear_persona(request):
     
     if request.method == 'POST':
-        nombre = request.POST['nombre']
-        apellido = request.POST['apellido']
-        persona = Humano(nombre=nombre, apellido=apellido, edad=random.randrange(1,99), fecha_creacion=datetime.now())
-        persona.save()
         
-        return redirect('ver_personas')
+        formulario = HumanoFormulario(request.POST)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+        
+            nombre = data['nombre']
+            apellido = data['apellido']
+            edad = data['edad']
+            fecha_creacion = data.get('fecha_creacion', datetime.now())
+            
+            persona = Humano(nombre=nombre, apellido=apellido, edad=edad, fecha_creacion=fecha_creacion)
+            persona.save()
+        
+            return redirect('ver_personas')
     
-    return render(request, 'Home/crear_persona.html', {})
+    formulario = HumanoFormulario()
+    
+    return render(request, 'Home/crear_persona.html', {'formulario': formulario})
 
 def ver_personas(request):
-    personas = Humano.objects.all()
-    return render(request, 'Home/ver_personas.html', {'personas': personas})
+    
+    nombre = request.GET.get('nombre')
+    
+    if nombre:
+        personas = Humano.objects.filter(nombre__icontains=nombre)
+    else:    
+        personas = Humano.objects.all()
+    
+    formulario = BusquedaHumanoFormulario()
+    
+    return render(request, 'Home/ver_personas.html', {'personas': personas, 'formulario': formulario})
 
 def index(request):
     return render(request, 'Home/index.html')
---
+++ README.md
@@ -0,0 +1,5 @@
+1. Levantar el servicio
+2. Crear y acceder a Home
+3. Crear personas
+4. Navegar por Ver personas
+
